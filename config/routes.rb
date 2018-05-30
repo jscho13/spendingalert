7a8df6cf8f5fbe48bde87f7d4c7a7dfbd4a16cc5
Joseph Cho <jscho13@gmail.com>
2018-05-30 10:50:34 -0400

    simplify scss and add tweak footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,8 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   get 'dashboard', to: 'welcome#dashboard'
-  resources :users
-
-	resources :subscriptions
-
-  get 'sample_index', to: 'welcome#sample_index'
-  get 'sample_generic', to: 'welcome#sample_generic'
   get 'sample_elements', to: 'welcome#sample_elements'
+
+  resources :users
+	resources :subscriptions
 end
