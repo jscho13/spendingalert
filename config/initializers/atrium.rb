4f69204011dbd84177bc504e1084ed7de06a8b68
Joseph Cho <jscho13@gmail.com>
2018-06-29 17:12:17 -0400

    use test accounts

--- config/initializers/atrium.rb
+++ config/initializers/atrium.rb
@@ -1,9 +1,9 @@
 ::Atrium.configure do |config|
   config.mx_api_key = ENV['MX_API_KEY']
   config.mx_client_id = ENV['MX_CLIENT_ID']
-	if !Rails.env.production?
+# 	if !Rails.env.production?
 		config.base_url = "https://vestibule.mx.com"
-	else
-		config.base_url = "https://atrium.mx.com" # base_url is set to "https://vestibule.mx.com" by default
-  end
+# 	else
+# 		config.base_url = "https://atrium.mx.com" # base_url is set to "https://vestibule.mx.com" by default
+#   end
 end
