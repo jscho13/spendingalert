dbbd84bf4447bc64e337775629b5eea23cc8e9f7
Joseph Cho <jscho13@gmail.com>
2018-06-29 16:34:38 -0400

    get text messages working

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,12 +1,4 @@
-
-if !Rails.env.production?
-  Twilio.configure do |config|
-    config.account_sid = ENV['TWILIO_DEVELOPMENT_ACCOUNT_SID']
-    config.auth_token = ENV['TWILIO_DEVELOPMENT_AUTH_TOKEN']
-  end
-else
-  Twilio.configure do |config|
-    config.account_sid = ENV['TWILIO_PRODUCTION_ACCOUNT_SID']
-    config.auth_token = ENV['TWILIO_PRODUCTION_AUTH_TOKEN']
-  end
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_PRODUCTION_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_PRODUCTION_AUTH_TOKEN']
 end
