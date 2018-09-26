2a19b61bf25dddebc55010fd9d5869525495c987
Joseph Cho <jscho13@gmail.com>
2018-09-26 18:53:39 -0400

    update complexity, add menu to edit page, condense dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :phone_number, phone: true
+  validates :phone_number, phone: true, :password_complexity
 
   attr_accessor :members,
                 :updated_total_spending,
@@ -80,6 +80,13 @@ HEREDOC
 
 
   private
+
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 
   def check_interval_5_days
     today = Date.today
