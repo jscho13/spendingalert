21137ad551bd2ccae0eacb5b3275c9cbf7108171
Joseph Cho <jscho13@gmail.com>
2018-11-18 18:38:21 -0500

    try configuring routes

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,9 +1,9 @@
 Twilio.configure do |config|
-  if !Rails.env.production?
-    config.account_sid = ENV['TWILIO_DEVELOPMENT_ACCOUNT_SID']
-    config.auth_token = ENV['TWILIO_DEVELOPMENT_AUTH_TOKEN']
-  else
+#   if !Rails.env.production?
+#     config.account_sid = ENV['TWILIO_DEVELOPMENT_ACCOUNT_SID']
+#     config.auth_token = ENV['TWILIO_DEVELOPMENT_AUTH_TOKEN']
+#   else
     config.account_sid = ENV['TWILIO_PRODUCTION_ACCOUNT_SID']
     config.auth_token = ENV['TWILIO_PRODUCTION_AUTH_TOKEN']
-  end
+#  end
 end
